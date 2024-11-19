99034b6bd9bd931bb5c298f7e7c34e4fcb6c00a7
imakshat47 <imakshat@github.com>
2024-11-19 21:57:16 +0530

    Model Parameters Added

--- flask/app.py
+++ flask/app.py
@@ -2,12 +2,13 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import pandas as pd
 
 # Initialize the Flask app
 app = Flask(__name__)
 
 # Load the trained model (assuming it's saved as 'model.pkl')
-# model = joblib.load('model.pkl')
+model = joblib.load('model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -15,10 +16,22 @@ def predict():
     data = request.get_json(force=True)
     
     # Make a prediction
-    # prediction = model.predict(features)
+    new_data = pd.DataFrame({
+        'Age': data['f1'],
+        'TotalDebt': data['f2'],
+        'MonthlyExpenses': data['f3'],
+        'NumberOfTransactions': data['f4'],
+        'TotalAmountLent': data['f5'],
+        'LastPaymentStatus':data['f6'],
+        'Wallet': data['f7'],
+    })
+
+    # Predict the credit score
+    predicted_score = model.predict(new_data)
+    # model.predict(features)
     
-    # Return the prediction as a JSON response prediction[0]
-    return jsonify({'prediction': data['f1']})
+    # Return the prediction as a JSON response
+    return jsonify({'credit_score': predicted_score[0]})
 
 if __name__ == '__main__':
     # Run the Flask app
